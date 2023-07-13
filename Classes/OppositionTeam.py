7153da1e59e84bba1291e8685aa615577e76a6d6
Bauyrzhan Zhanuzakov <zhanuzakovb@yahoo.com>
2023-07-13 13:34:13 +0200

    Updated OppositionTeam Renamed BankOfOptions to OptionsBank

--- Classes/OppositionTeam.py
+++ Classes/OppositionTeam.py
@@ -1,23 +1,22 @@
 class OppositionTeam:
-    def init(self, name, options, description):
+    def init(self, name, description=None, objectives=None):
         self.name = name
-        self.options = options
         self.description = description
+        self.objectives = objectives
 
-    def get_name(self):
-        return self.name
-
-    def set_name(self, name):
-        self.name = name
-
-    def get_options(self):
+    def get_options(self, OppBankOfOptions): # i need to fix this one
         return self.options
+    
+    def choose_option(self, OppOptions): #PropOptions will be an instance of OptionsBank which is passed to the Proposition Team class method of chosing option
+        print(OppOptions)
+        print('Team {self.name}, choose your option')
+        selected_option = input('Enter the option number: ')
+        selected_index = OppOptions[int(selected_option)-1]  #selected_index is the selection of an option by Proposition team
 
-    def set_options(self, options):
-        self.options = options
+        if selected_option != 0 or selected_option <= len(self.options):
+            print('Team {self.name} chose option {selected_option}')
+        else:
+            print("Invalid option. Chose one of the available options") # needs redirection
+            return None
 
-    def get_description(self):
-        return self.description
-
-    def set_description(self, description):
-        self.description = description
+ 
